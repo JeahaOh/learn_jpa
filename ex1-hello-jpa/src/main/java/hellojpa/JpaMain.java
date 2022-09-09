2b152495077285559e4cb791078e523d83cbc09e
JeahaOh <jeaha1217@gmail.com>
2022-09-09 15:19:28 +0900

    일대다 (1:N) 단반향 정리 - 일대다 단방향은 일대다(1:N)에서 1이 연관관계 주인. - 테이블 일대다 관계는 항상 다(N) 쪽에 외래키가 있음 - 객체와 테이블의 차이 때문에 반대편 테이블의 외래 키를 관리하는 특이한 구조 - @JoinColumn을 꼭 사용해야 함. 그렇지 않으면 조인 테이블 방식을 사용함(중간에 테이블을 하나 추가함) - 일대다 단방향 매핑의 단점   - 엔티티가 관리하는 외래 키가 다른 테이블에 있음   - 연관관계 관리를 위해 추가로 UPDATE SQL 실행 - 일대다 단방향 매핑보다는 다대일 양방향 매핑을 사용하자 - 읽기 전용 필드를 사용해서 양방향 처럼 사용하는 방법   - @JoinColumn(insertable=false, updateable=false)   0 공식적이지 않음 - 다대일 양방향을 사용하도록

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -16,47 +16,18 @@ public class JpaMain {
     tx.begin();
     
     try {
-      // 양방향 매핑시 가장 많이 하는 실수 1
-      // 연관관계의 주인에 값을 입력하지 않음
-      Member memberA = new Member();
-      memberA.setUsername("member 1");
-      em.persist(memberA);
+      Member member = new Member();
+      member.setUsername("member1");
       
-      Team teamA = new Team();
-      teamA.setName("TeamA");
-      // 역방향(주인이 아닌 방향)만 연관관계 설정
-      teamA.getMembers().add(memberA);
-      em.persist(teamA);
-  
-      em.flush();
-      em.clear();
+      em.persist(member);
       
-      // 양방향 매핑시 가장 많이하는 실수 2
-      Team teamB = new Team();
-      teamB.setName("Team B");
-      em.persist(teamB);
+      Team team = new Team();
+      team.setName("teamA");
+      //
+      team.getMembers().add(member);
       
-      Member memberB = new Member();
-      memberB.setUsername("member b");
-      //memberB.setTeam(teamB);
-      memberB.changeTeam(teamB);
-      em.persist(memberB);
+      em.persist(team);
       
-      // 양방향으로 값을 다 셋팅 해 줘야 함
-      //teamB.getMembers().add(memberB);
-      
-      // em.flush();
-      // em.clear();
-      
-      // 1차 캐시
-      Team findTeam = em.find(Team.class, teamB.getId());
-      List<Member> membersB = findTeam.getMembers();
-      
-      System.out.println("-".repeat(20));
-      for (Member m : membersB) {
-        System.out.println("m : " + m.getUsername());
-      }
-      System.out.println("-".repeat(20));
       
       tx.commit();
     } catch (Exception e) {
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -13,9 +13,9 @@ public class Member {
   @Column(name ="USERNAME")
   private String username;
   
-  @ManyToOne
-  @JoinColumn(name = "TEAM_ID")
-  private Team team;
+//  @ManyToOne
+//  @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
+//  private Team team;
   
   public Long getId() {
     return id;
@@ -31,13 +31,5 @@ public class Member {
   
   public void setUsername(String name) {
     this.username = name;
-  }
-  
-  public Team getTeam() {
-    return team;
-  }
-  
-  public void setTeam(Team team) {
-    this.team = team;
   }
 }
